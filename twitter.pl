54012ae6be22833d61f22ca3609e3771439ff498
vitrio wibisono <akito.evol@gmail.com>
2012-04-17 14:17:30 +0700

    add json parser for each event

--- twitter.pl
+++ twitter.pl
@@ -3,14 +3,14 @@
 use JSON;
 use Encode;
 
-$json = new JSON;
-$json = $json->allow_nonref([$enable]);
-$json = $json->relaxed([$enable]);
-$json = $json->max_depth([$maximum_nesting_depth]);
-
 $file_log = $ARGV[0];
 $file_conf = $ARGV[1];
 
+$json = new JSON;
+
+$json = $json->allow_nonref([$enable]);
+$json = $json->relaxed([$enable]);
+$json = $json->max_depth([$maximum_nesting_depth]);
 
 open (FILE, $file_log) or die("Unable to open file log!! " .$file_log . "\n");
 #open (CONFIG, $file_conf) or die ("Unable to open file config!! ". $file_conf . "\n");
@@ -22,18 +22,15 @@ while (<FILE>)
     print "Parsing line: $counter\n";
     $counter + 1;
 
-    my ($url, $res_txt) = $line =~/URL:(.*),RES_TXT:(.*)/;
-    my ($url, $res_txt) = ($1,$2);
-    
-    #print $res_txt;
+    my ($url, $res_txt) = $line =~ /URL:(.*),RES_TXT:(.*),<EOH>/;
+    my ($url, $res_txt) = ($1,$2);   
 
-    my $obj = $json->utf8(1)->decode($res_txt);    
+    my $obj = $json->utf8(1)->decode($res_txt); 
 
     if($url = ~/.*\/(.*)\.json/){
         my $event = $1;
 
-        if ($event eq 'user_timeline'){
-            print $event . "\n";
+        if ($event eq 'user_timeline'){ 
 
             for my $user_timeline (@$obj)
             {   
@@ -41,9 +38,11 @@ while (<FILE>)
                 print $user_timeline->{user}{name} . "\n";
                 print $user_timeline->{screen_name} . "\n"; 
                 print $user_timeline->{text} . "\n";
-            }    
+            }   
 
-            # my $created_at = $res_txt = ~/.*created_at":.{2}(.*?)",/;
+            # uncomment this if you want to hardcorely regex
+
+            # my $created_at = $res_txt =~ /.*created_at":.{2}(.*?)",/;
             # my $created_at = $1;
             # print "created_at: " . $created_at . "\n";
 
@@ -55,7 +54,7 @@ while (<FILE>)
             # my $name = $1;
             # print "name: " . $name . "\n";
 
-            # while ($res_txt = ~/"name":[\s]"(.*?)",/)
+            # while ($res_txt =~ /"name":[\s]"(.*?)",/g)
             # {
             #     print $1 . "\n";
             # }
@@ -103,29 +102,110 @@ while (<FILE>)
             # my $retweeted = $res_txt = ~/.*retweeted":[\s|\S]([\d|\w]*),/;
             # my $retweeted = $1;      
             # print "retweeted: " . $retweeted . "\n";
-            
+
+            print "====================================================\n";
         }
         elsif ($event eq 'update')
         {
-            print $event . "\n\n\n";
-            print $obj->{text} . "\n";
-            print $obj->{id} . "\n";
-            print $obj->{source} . "\n";
+            my $update = $obj;
+                print $update->{coordinates} . " ";
+                print $update->{created_at} . " ";
+                print $update->{favorited} . " ";
+                print $update->{id} . " ";
 
+                        # here go the entities
+                        # hash tags e.g. #dafuq! #ohgodwhy #bitchplease #notbad
+                        foreach my $entity (@{$update->{entities}->{hashtags}})
+                            {
+                                print $entity->{text} . "\n";
+                            }
+
+                        foreach my $entity (@{$update->{entities}->{user_mentions}})
+                        {
+                            print $entity->{name} . "\n";
+                            print $entity->{id} . "\n";
+                            print $entity->{screen_name} . "\n";
+                        }
+
+                print $update->{text} . " ";
+                print $update->{in_reply_to_user_id_str} . " ";
+                print $update->{retweet_count} . " ";
+                print $update->{retweeted} . " ";
+                print $update->{source} . " ";
+                print $update->{in_reply_to_screen_name} . " ";
+
+                    print $update->{user}->{id} . " ";
+                    print $update->{user}->{name} . " ";
+                    print $update->{user}->{screen_name} . " ";
+                    print $update->{user}->{time_zone} . "\n";
+
+                print $update->{place} . " ";
+                print $update->{in_reply_to_status_id} . " \n";
+
+            print "====================================================\n";
         }
         elsif ($event eq 'mentions')
         {
-            print $event . "\n\n\n";
+            # null value wont be printed
+            # one mention loop
+            foreach my $mention(@$obj){
+                print $mention->{coordinates} . "\n";
+                print $mention->{favorited} . "\n";
+                print $mention->{created_at} . "\n";
 
-            for my $mentions (@$obj)
-            {   
-                print $mentions->{id} . "\n";
-                print $mentions->{user}{name} . "\n";
-                print $mentions->{text} . "\n";
-                print $mentions->{entities}{user_mentions} . "\n";
-            } 
+                        foreach my $entity (@{$mention->{entities}->{user_mentions}})
+                        {
+                            print $entity->{name} . "\n";
+                            print $entity->{id} . "\n";
+                            print $entity->{screen_name} . "\n";
+                        }
+                
+                print $mention->{text} . "\n";
+                print $mention->{id} . "\n";
+                print $mention->{geo} . "\n";
+                print $mention->{in_reply_to_user_id} . "\n";
+                print $mention->{place} . "\n";
+                print $mention->{in_reply_to_screen_name} . "\n";
 
-            print "\n\n\n\n";
+                    print $mention->{user}->{name} . "\n";
+                    print $mention->{user}->{created_at} . "\n";
+                    print $mention->{user}->{profile_image_url} . "\n";
+                    print $mention->{user}->{location} . "\n";
+                    print $mention->{user}->{url} . "\n";
+                    print $mention->{user}->{favourites_count} . "\n";
+                    print $mention->{user}->{protected} . "\n";
+                    print $mention->{user}->{id} . "\n";
+                    print $mention->{user}->{followers_count} . "\n";
+                    print $mention->{user}->{time_zone} . "\n";
+                    print $mention->{user}->{description} . "\n";
+                    print $mention->{user}->{statuses_count} . "\n";
+                    print $mention->{user}->{friends_count} . "\n";
+                    print $mention->{user}->{screen_name} . "\n";
+
+                print $mention->{source} . "\n";
+                print $mention->{in_reply_to_status_id} . "\n";
+
+                print "+-+-+-+-+-+-+-+-+-+END OF MENTION+-+-+-+-+-+-+-+-+-+\n"
+            }
+
+            # while ($res_txt =~ /"coordinates":\s(.*),/g)
+            # {
+            #       print $1 . "\n";
+            # }
+
+            # while ($res_txt =~ /"created_at":\s(.*?),/g)
+            # {
+            #     print $1 . "\n";
+            # }
+
+            # while ($res_txt =~ /"name":\s"(.*?)",/g)
+            # {
+            #     print $1 . "\n";
+            # }
+
+            # while ($res_txt =~ /"screen_name":\s"(.*?)",/g)
+
+            print "====================================================\n";
 
         }
     }
