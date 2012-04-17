460520a7956d77d4221abaf86461b3d5683c8c8b
vitrio wibisono <akito.evol@gmail.com>
2012-04-17 18:33:38 +0700

    check if $obj is a json array or not, refactor the code, remove regex hardcore and use json parser, cleaning comments, and fix event log

--- twitter.pl
+++ twitter.pl
@@ -35,10 +35,10 @@ while (<FILE>)
 
     if($url =~ /.*\/(.*)\.json/){
         my $event = $1;
-    
-        if(ref($obj) eq "ARRAY") {
 
-            print CSV "$event,";
+        print CSV "$event,";
+
+        if(ref($obj) eq "ARRAY") {
 
             foreach my $non_entity(@$obj){
 
@@ -84,39 +84,39 @@ while (<FILE>)
             } 
         }
         else {
-                print CSV $non_entity->{coordinates} ." " .
-                $non_entity->{created_at} . " " .
-                $non_entity->{favorited} . " " . 
-                $non_entity->{text} . " " .
-                $non_entity->{id} . " " .
-                $non_entity->{geo} . " " .
-                $non_entity->{in_reply_to_user_id} . " " .
-                $non_entity->{place} . " " .
-                $non_entity->{in_reply_to_screen_name} . " " .
-                $non_entity->{name} . " " .
-                $non_entity->{screen_name} . " " .
-                $non_entity->{statuses_count} . " " .
-                $non_entity->{listed_count} . " " .
+                print CSV $obj->{coordinates} ." " .
+                $obj->{created_at} . " " .
+                $obj->{favorited} . " " . 
+                $obj->{text} . " " .
+                $obj->{id} . " " .
+                $obj->{geo} . " " .
+                $obj->{in_reply_to_user_id} . " " .
+                $obj->{place} . " " .
+                $obj->{in_reply_to_screen_name} . " " .
+                $obj->{name} . " " .
+                $obj->{screen_name} . " " .
+                $obj->{statuses_count} . " " .
+                $obj->{listed_count} . " " .
 
-                $non_entity->{user}->{name} . " " .
-                $non_entity->{user}->{created_at} . " " .
-                $non_entity->{user}->{profile_image_url} . " " .
-                $non_entity->{user}->{location} . " " .
-                $non_entity->{user}->{url} . " " .
-                $non_entity->{user}->{favourites_count} . " " .
-                $non_entity->{user}->{protected} . " " .
-                $non_entity->{user}->{id} . " " .
-                $non_entity->{user}->{followers_count} . " " .
-                $non_entity->{user}->{time_zone} . " " .
-                $non_entity->{user}->{description} . " " .
-                $non_entity->{user}->{statuses_count} . " " .
-                $non_entity->{user}->{friends_count} . " " .
-                $non_entity->{user}->{screen_name} . " " .
+                $obj->{user}->{name} . " " .
+                $obj->{user}->{created_at} . " " .
+                $obj->{user}->{profile_image_url} . " " .
+                $obj->{user}->{location} . " " .
+                $obj->{user}->{url} . " " .
+                $obj->{user}->{favourites_count} . " " .
+                $obj->{user}->{protected} . " " .
+                $obj->{user}->{id} . " " .
+                $obj->{user}->{followers_count} . " " .
+                $obj->{user}->{time_zone} . " " .
+                $obj->{user}->{description} . " " .
+                $obj->{user}->{statuses_count} . " " .
+                $obj->{user}->{friends_count} . " " .
+                $obj->{user}->{screen_name} . " " .
 
-                $non_entity->{status}->{text} . " " .
-                $non_entity->{status}->{place}->{name} . " " .
-                $non_entity->{status}->{place}->{country_code} . " " .
-                $non_entity->{status}->{place}->{country} . " " ;            
+                $obj->{status}->{text} . " " .
+                $obj->{status}->{place}->{name} . " " .
+                $obj->{status}->{place}->{country_code} . " " .
+                $obj->{status}->{place}->{country} . " " ;            
         }
         print CSV "\n";
     }
