ee4eba7f8abb00e32e959443d1b0398ea4a23ddd
vitrio wibisono <akito.evol@gmail.com>
2012-04-23 21:07:01 +0700

    add csv filename based on unix timestamp, separate the output regex from $line to $res_txt and $url and this could be added as needed, fix error printing hash value in csv because of redundant hash e.g. $non_entity->{staus}->{place}->{place}, add std error while opening file config and log, fix printing csv functions.

--- twitter.pl
+++ twitter.pl
@@ -19,7 +19,9 @@ $json = $json->allow_nonref([$enable]);
 $json = $json->relaxed([$enable]);
 $json = $json->max_depth([$maximum_nesting_depth]);
 
-open (CSV, ">>text");
+my $unix_time = time();
+
+open (CSV, ">>text"); # change with $unix_time in production
 
 while (<FILE>)
 {
@@ -28,29 +30,21 @@ while (<FILE>)
     print "Parsing line: $counter\n";
     $counter + 1;
 
-    my ($ts, $saddr, $daddr, $sport, $dport, $fid, $mtd, $resp, $host, $uac, $url, $cookie, $req_len, $req_type, $res_len, $res_type, $res_cencode, $res_tencode, $req_txt, $res_txt) = $line =~ /TS:(.*),SADDR:(.*),DADDR:(.*),SPORT:(.*),DPORT:(.*),FID:(.*),MTD:(.*),RESP:(.*),HOST:(.*),UAC:(.*),URL:(.*),COOKIE:(.*),REQ_LEN:(.*),REQ_TYPE:(.*),RES_LEN:(.*),RES_TYPE:(.*),RES_CENCODE:(.*),RES_TENCODE:(.*),REQ_TXT:(.*),RES_TXT:(.*),<EOH>/;
+    # my $ts = $line =~ /TS:(.*),/;
+    my $url = $line =~ /URL:(.*?),/;
+    my $url = $1;
 
-    my ($ts, $saddr, $daddr, 
-             $sport, $dport, 
-             $fid, $mtd, $resp, $host, $uac, $url, $cookie, 
-             $req_len, $req_type, 
-             $res_len, $res_type, $res_cencode, $res_tencode, 
-             $req_txt, $res_txt) = ($1, $2, $3, 
-                                        $4, $5, 
-                                        $6, $7, $8, $9, $10, $11, $12, 
-                                        $13, $14, 
-                                        $15, $16, $17, $18, 
-                                        $19, $20); 
+    my $res_txt = $line =~ /RES_TXT:(.*),<EOH>/;
+    my $res_txt = $1;
 
     my $obj = $json->utf8(1)->decode($res_txt); 
 
-    my $encoded_url = uri_unscape($url);
-    print $encoded_url . "\n\n\n\n";
+    my $encoded_url = uri_unescape($url);
 
     if($url =~ /.*\/(.*)\.json/){
         my $event = $1;
 
-        print CSV "$ts $sv $event $sv";
+        print CSV "$event $sv";
 
         if(ref($obj) eq "ARRAY") {
             foreach my $non_entity(@$obj){
@@ -60,7 +54,7 @@ while (<FILE>)
                     print_non_entity($Config->{twitter}->{$index}, $non_entity, $sv);
                     $index++;
                 }
-                    # entities are fix by twitter
+                    # entities are absolute from twitter
                     foreach my $entity (@{$non_entity->{entities}->{user_mentions}})
                     {
                           print_entity('id','name','screen_name', $entity, $sv);
@@ -73,7 +67,7 @@ while (<FILE>)
                 print_non_entity($Config->{twitter}->{$index}, $obj, $sv);
                 $index++
             }
-            # entities are fixed by twitter              
+            # entities are absolute from twitter              
             foreach $entities ($obj->{entities}->{user_mentions})
             {
                 foreach $entity(@{$entities})
@@ -93,12 +87,12 @@ sub print_non_entity {
 #           2. non_entity->user->index 
 #           3. non_entity->status->index 
 #           4. non_entity->status->place->index
-    print CSV $_[1]->{$_[0]} . $_[2] . 
-              $_[1]->{user}->{$_[0]} . $_[2] . 
-              $_[1]->{status}->{$_[0]} . $_[2] . 
-              $_[1]->{status}->{place}->{$_[0]} . $_[2] . 
-              $_[1]->{recipient}->{$_[0]} . $_[2] . 
-              $_[1]->{sender}->{$_[0]} . $_[2];
+    print CSV $_[1]->{$_[0]} . $_[2] ;
+    print CSV $_[1]->{user}->{$_[0]} . $_[2] ; 
+    print CSV $_[1]->{status}->{$_[0]} . $_[2] ; 
+    print CSV $_[1]->{status}->{place}->{$_[0]} . $_[2] ; 
+    print CSV $_[1]->{recipient}->{$_[0]} . $_[2] ;
+    print CSV $_[1]->{sender}->{$_[0]} . $_[2];
 }
 sub print_entity {
     print CSV $_[3]->{$_[0]} . $_[4] . 
